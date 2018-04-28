4c7c858f9b1ae04f9be58f6ec6b69a9d2657b7bc
Tristan Dubé <tristan.dube@gmail.com>
2018-04-28 10:14:03 -0400

    Started adding classes to handle loading different types of files.

---
+++ src/framework/loaders/shaderloader.cpp
@@ -0,0 +1,17 @@
+#include "shaderloader.h"
+
+#include <string>
+#include <cstdio>
+
+ShaderLoader::ShaderLoader()
+{
+}
+
+ShaderLoader::~ShaderLoader()
+{
+}
+
+char *ShaderLoader::LoadShader(char *path)
+{
+
+}
---
+++ src/framework/loaders/shaderloader.h
@@ -0,0 +1,15 @@
+#pragma once
+
+class ShaderLoader
+{
+public:
+    ShaderLoader();
+
+    ~ShaderLoader();
+
+    char* LoadShader(char* path);
+
+private:
+};
+
+
