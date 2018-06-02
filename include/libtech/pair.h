ccd6d22f352bbde00b5dd4bc61a2359af9b53803
Tristan Dubé <tristan.dube@gmail.com>
2018-06-02 10:58:35 -0400

    Added some types from CommsFramework

---
+++ include/libtech/pair.h
@@ -0,0 +1,23 @@
+#pragma once
+
+template <class T, class Y>
+class Pair
+{
+public:
+    Pair()
+    {
+    };
+
+    Pair(T item1, Y item2)
+    {
+        this->Item1 = item1;
+        this->Item2 = item2;
+    }
+
+    ~Pair()
+    {
+    };
+
+    T Item1;
+    Y Item2;
+};
--- include/libtech/vec.h
+++ include/libtech/vec.h
@@ -1,5 +1,11 @@
 #pragma once
 
+struct vec2
+{
+    float x;
+    float y;
+};
+
 struct vec3
 {
     float x;
