c70d8505b01e8b64c523f0197e0e2aa6b3304530
Tristan Dubé <tristan.dube@gmail.com>
2018-06-02 20:27:37 -0400

    Added Geometry structures.
    Those structs  were moved here from the MicroEngine project.

--- include/libtech/Geometry.h
+++ include/libtech/Geometry.h
@@ -5,3 +5,39 @@
 LIBTECH_API double deg2rad(double deg);
 
 LIBTECH_API double rad2deg(double deg);
+
+struct LIBTECH_CLASS FPosition
+{
+    float X;
+    float Y;
+
+    FPosition()
+    {
+        X = 0;
+        Y = 0;
+    }
+
+    FPosition(float x, float y)
+    {
+        X = x;
+        Y = y;
+    }
+};
+
+struct LIBTECH_CLASS FSize
+{
+    float Width;
+    float Height;
+
+    FSize()
+    {
+        Width = 0;
+        Height = 0;
+    }
+
+    FSize(float w, float h)
+    {
+        Width = w;
+        Height = h;
+    }
+};
