05eac6fe71b313c176a1f3ee0707d32799808511
Tristan Dubé <tristan.dube@gmail.com>
2018-07-03 21:00:32 -0400

    Fix linker error in the pathutils.h file.
    I forgot to include libtech.h and add the LIBTECH_API like an idiot.

--- include/libtech/pathutils.h
+++ include/libtech/pathutils.h
@@ -2,31 +2,33 @@
 
 #include <list>
 
+#include "libtech.h"
+
 /**
  * Get the name part of a path containing the extension.
  */
-char* get_file_name(const char* path);
+LIBTECH_API char* get_file_name(const char* path);
 
 /**
  * Get the name part of a path without the extension part.
  */
-char* get_file_name_noext(const char* path);
+LIBTECH_API char* get_file_name_noext(const char* path);
 
 /**
  * Get the extension part in a path containing the '.' .
  */
-char* get_file_extension(const char* path);
+LIBTECH_API char* get_file_extension(const char* path);
 
 /**
  * Get the path to the parent directory.
  */
-char* get_parent_directory_path(const char* path);
+LIBTECH_API char* get_parent_directory_path(const char* path);
 
 /**
  * Get each folder part of the path structure.
  * Everything in between the slashes is put into the list.
  */
-std::list<char*>* get_path_parts(const char* path);
+LIBTECH_API std::list<char*>* get_path_parts(const char* path);
 
 /**
  * Replace backslashes with forward slashes.
